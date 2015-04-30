29a5b7ee301ce9522db240a7a457809769f236b5
Basavaraj Teli <basavaraj.telicse@gmail.com>
2015-04-30 14:22:01 +0530

    Declared all objects in single var

--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,15 @@
 'use strict';
 console.log('inside api.js');
-var metamodels = require('../models/metamodels.js');
+var metamodels = require('../models/metamodels.js'),
+    models = metamodels(),
+    express = require('express'),
+    restful = require('node-restful'),
+    mongoose = restful.mongoose,
+    router = express.Router(),
+    schemas = models.modules,
+    mongooseschema = [],
+    restfulmodel = [];
 
-var models = metamodels();
-var express = require('express');
-var restful = require('node-restful');
-
-var mongoose = restful.mongoose;
-var router = express.Router();
-
-var schemas = models.modules;
-
-var mongooseschema = [];
-var restfulmodel = [];
-router.index = function(req, res){
-  res.render('index');
-};
-
-router.partials = function (req, res) {
-  var name = req.params.name;
-  res.render('partials/' + name);
-};
 //set up the api
 for(var i=0; i<schemas.length; i++){
 	console.log ('creating api: ' + schemas[i].modelname);
