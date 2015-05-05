a39b22b554be4a7ab32d5f0bbcf55dcf1fbe2d4f
Basavaraj Teli <basavaraj.telicse@gmail.com>
2015-05-05 13:55:30 +0530

    Added CRUD functionality.

--- public/js/app.js
+++ public/js/app.js
@@ -10,18 +10,89 @@ angular.module('app',['app.services']).config(function($locationProvider,$routeP
       templateUrl: 'partials/truck',
       controller: 'truckCtrl'
   })
-    .otherwise({
+      .when('/addtruck', {
+      templateUrl: 'partials/addTruck',
+      controller: 'truckCtrl'
+  })
+      .when('/addcar', {
+      templateUrl: 'partials/addCar',
+      controller: 'carCtrl'
+  }) .otherwise({
       redirectTo:'/'
   });
-}).controller('carCtrl', function($scope,VehicleDetails){
-    
+}).controller('carCtrl', function($scope,VehicleDetails,$location){
+    $scope.carData=function(){
+        
     VehicleDetails.query({vehicle:'car'}, function(response){
-        $scope.carsData = response[0];
+        $scope.carsData = response;
     });
+    };
+    $scope.carData();  
+    $scope.add=function(){        
+        VehicleDetails.post({vehicle:'car','name':$scope.name,'model':$scope.model,'company':$scope.company,'yearofmanufacturing':$scope.yom}, function(response){
+            $scope.ss = response;
+            if($scope.ss){
+            $location.path('#/');
+            }
+        });
+    };
+    
+    $scope.edit=function(id){        
+    VehicleDetails.get({vehicle:'car',entryId:id}, function(response){
+        $scope.card = response;
+    });
+    };
+    
+    $scope.update=function(uid){     VehicleDetails.update({vehicle:'car','name':$scope.card.name,'model':$scope.card.model,'company':$scope.card.company,'yearofmanufacturing':$scope.card.yearofmanufacturing,entryId:uid}, function(response){
+            $location.path('#/');
+        });  
+        $scope.carData();
+        $scope.card='';
+    };
+    
+        $scope.delete=function(id){        
+    VehicleDetails.remove({vehicle:'car',entryId:id}, function(response){
+       $location.path('#/');
+       $scope.carData();    
+    });
+    };
 
-}).controller('truckCtrl', function($scope,VehicleDetails){
-    VehicleDetails.query({vehicle:'truck'}, function(response){
-        $scope.truckData = response[0];
+
+}).controller('truckCtrl', function($scope,VehicleDetails,$location){
+    $scope.truckData=function(){
+            VehicleDetails.query({vehicle:'truck'}, function(response){
+        $scope.truckData = response;
+    });    
+    };
+    $scope.truckData();
+    
+    $scope.add=function(){        
+        VehicleDetails.post({vehicle:'truck','name':$scope.name,'model':$scope.model,'company':$scope.company,'weightcapacity':$scope.yom}, function(response){
+            $scope.ss = response;
+            if($scope.ss){
+            $location.path('#/');
+            }
+        });
+    };
+    
+    $scope.edit=function(id){        
+    VehicleDetails.get({vehicle:'truck',entryId:id}, function(response){
+        $scope.truckd = response;
     });
+    };
+    
+    $scope.update=function(uid){     VehicleDetails.update({vehicle:'truck','name':$scope.truckd.name,'model':$scope.truckd.model,'company':$scope.truckd.company,'weightcapacity':$scope.truckd.weightcapacity,entryId:uid}, function(response){
+            $location.path('#/');
+            $scope.truckData();
+            $scope.truckd='';
+        });    
+    };
+    
+        $scope.delete=function(id){        
+    VehicleDetails.remove({vehicle:'truck',entryId:id}, function(response){
+       $location.path('#/');
+        $scope.truckData();
+    });
+    };
 
 });
--- public/js/services.js
+++ public/js/services.js
@@ -5,9 +5,10 @@ angular.module('app.services', ['ngResource'])
 .factory('VehicleDetails', function($resource) {    
     return $resource('/api/:vehicle/:entryId', {}, {
         query: {method:'GET', params:{vehicle:'@vehicle',entryId:''}, isArray:true},
-        post: {method:'POST'},
-        update: {method:'PUT', params: {entryId: '@entryId'}},
-        remove: {method:'DELETE'}
-    });    
+        post: {method:'POST',params: {vehicle: '@vehicle'}},
+        update: {method:'PUT', params: {vehicle: '@vehicle',entryId: '@entryId'}},
+        remove: {method:'DELETE',params: {vehicle: '@vehicle',entryId: '@entryId'}}
+    }
+    );    
 /*  return $resource('http://localhost\\:3000/api/car');*/
 });
